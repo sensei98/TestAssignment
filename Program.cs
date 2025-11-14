22e06d94cbac8f91a4776f6baad01ec31eb9e33c
tsaglisamuel <schaltzchagly@gmail.com>
2025-11-14 18:53:06 +0100

    comments added, naming sorted

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ public record Listing(Makelaar Makelaar, bool hasGarden);
 
 public class Program
 {
+    //user secrets 
     public static async Task Main()
     {
         var config = new ConfigurationBuilder()
@@ -26,31 +27,36 @@ public static class APIAnalyzer
     private const int RequestDelayinMilliSeconds = 600; //100 requests p/m
     //configuration 
     
+    
     public static async Task Run(SConfig config)
     {
-
-        var amsterdamListings = await fetchAllListings(config,"/amsterdam");
-        var gardenListings = await fetchAllListings(config,"/amsterdam/tuin");
+        var amsterdamListings = await FetchAllListings(config,"/amsterdam");
+        var gardenListings = await FetchAllListings(config,"/amsterdam/tuin");
 
         DisplayTop10Listings("TOP 10 MAKELAARS IN AMSTERDAM", CountListings(amsterdamListings));
         DisplayTop10Listings("TOP 10 MAKELAARS IN AMSTERDAM WITH GARDEN", CountListings(gardenListings));
         
     }
 
-    private static async Task<List<Listing>> fetchAllListings(SConfig config,string query)
+    private static async Task<List<Listing>> FetchAllListings(SConfig config,string query)
     {
         var allListings = new List<Listing>();
         int page = 1;
 
+        //processing all pages 
         while (true)
         {
+            //fetching a single page of results from API
             var response = await FetchPage(config,query, page);
+            //if API returns no objects or returns a null response
             if (response?.Objects == null || !response.Objects.Any()) break;
 
+            //collect all makelaar object results from api and check if the query contains "tuin and add to list for results with gardens"
             allListings.AddRange(response.Objects.Select(
-                obj => new Listing(new Makelaar(obj.MakelaarId, obj.MakelaarNaam),
+                obj => new Listing(new Makelaar(obj.MakelaarId, obj.MakelaarNaam),  
                     query.Contains("tuin"))));
 
+            // if >= total page 25 
             if (page >= response.Paging.TotalPages) break;
             page++;
         }
@@ -58,15 +64,15 @@ public static class APIAnalyzer
         return allListings;
     }
 
-    private static async Task<ApiResponse?> FetchPage(SConfig config, string query, int page)
+    private static async Task<APIResponse?> FetchPage(SConfig config, string query, int page)
     {
-        await RateLimit();
+        await APIRateLimit();
 
         var url = $"{config.BASEURL}{config.APIKEY}/?type=koop&zo={query}&page={page}&pagesize=25";
         try
         {
             var json = await client.GetStringAsync(url);
-           return JsonSerializer.Deserialize<ApiResponse>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+           return JsonSerializer.Deserialize<APIResponse>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
 
         }
         catch (Exception ex)
@@ -77,8 +83,9 @@ public static class APIAnalyzer
         }
     }
 
-    private static async Task RateLimit()
+    private static async Task APIRateLimit()
     {
+        //checking for limits for 100 requests per minute for preventing 429 errors
         var timeSinceLastApiCall = (DateTime.Now - lastRequest).TotalMilliseconds;
         if (timeSinceLastApiCall < RequestDelayinMilliSeconds)
         {
@@ -90,10 +97,15 @@ public static class APIAnalyzer
 
     private static List<(string Name, int Count)> CountListings(List<Listing> listings)
     {
-        var grouped = listings.GroupBy(listing => listing.Makelaar.Name);
-        var counted = grouped.Select(grouping => (Name: grouping.Key, Count: grouping.Count()));
-        var sorted = counted.OrderByDescending(x => x.Count);
+        //grouping by makelaar name
+        var groupedByName = listings.GroupBy(listing => listing.Makelaar.Name);
+        //(Name: Count) for each makelaar 
+        var groupedCount = groupedByName.Select(grouping => (Name: grouping.Key, Count: grouping.Count()));
+        //sorting by highest to lowest 
+        var sorted = groupedCount.OrderByDescending(x => x.Count);
+        //taking the first 10 from the list
         var top10 = sorted.Take(10).ToList();
+
         return top10;
     }
 
@@ -108,13 +120,13 @@ public static class APIAnalyzer
     }
 }
 
-    public class ApiResponse
+    public class APIResponse
     {
-        public List<PropertyObject> Objects { get; set; }
+        public List<APIObject> Objects { get; set; }
         public PagingInfo Paging { get; set; }
     }
 
-    public class PropertyObject
+    public class APIObject
     {
         public int MakelaarId { get; set; }
         public string MakelaarNaam { get; set; }
